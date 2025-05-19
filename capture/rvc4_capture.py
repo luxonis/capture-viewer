99f215fbf3fe4abd2c897368002df9c1ea9c31d7
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2025-05-19 11:33:39 +0200

    rvc4 capture - add exposure settings

--- capture/rvc4_capture.py
+++ capture/rvc4_capture.py
@@ -141,12 +141,16 @@ def controlQueueSend(input_queues, ctrl):
         queue.send(ctrl)
 
 def initialize_mono_control(settings):
-    mono_settings = settings["monoSettings"]
     ctrl = dai.CameraControl()
 
+    mono_settings = settings["monoSettings"]
     ctrl.setLumaDenoise(mono_settings["luma_denoise"])
     ctrl.setSharpness(mono_settings["sharpness"])
     ctrl.setContrast(mono_settings["contrast"])
+
+    exposure_settings = settings["exposureSettings"]
+    if not exposure_settings["autoexposure"]:
+        ctrl.setManualExposure(exposure_settings["expTime"], exposure_settings["sensIso"])
 
     return ctrl
 
