f000bc74293c368ee34299aebd1db1ecf211e95a
CasualMathEnjoyer <morovicsova.katka@gmail.com>
2025-07-18 13:47:11 +0200

    move pipeline

--- capture/dai3_stereo_capture_alternating.py
+++ capture/dai3_stereo_capture_alternating.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-import queue
 
-import cv2
-import os
 import depthai as dai
 import numpy as np
 import argparse
+import datetime
 import time
 import json
-import datetime
+import cv2
+import os
 
 print(dai.__version__)
 
@@ -20,115 +19,10 @@ if str(dai.__version__)[0] != "3":
 script_dir = os.path.dirname(os.path.abspath(__file__))
 root_path = os.path.join(os.path.dirname(script_dir), 'DATA')
 
-from utils.capture_universal import initialize_capture, finalise_capture, count_output_streams
 from utils.raw_data_utils import unpackRaw10
 from utils.show_frames import visualize_frame, visualize_frame_info
-
-def set_stereo_node(pipeline, settings):
-    stereo = pipeline.create(dai.node.StereoDepth)
-
-    stereo.setRectification(True)
-
-
-    stereo.setDefaultProfilePreset(eval(f"dai.node.StereoDepth.PresetMode.{settings['profilePreset']}"))
-
-    stereo.setLeftRightCheck(settings["LRcheck"])
-    if settings["extendedDisparity"]: stereo.setExtendedDisparity(True)
-    else: stereo.setExtendedDisparity(False)
-    # if settings["subpixelDisparity"]: stereo.setSubpixel(True)
-    # if settings["subpixelValue"]: stereo.setSubpixelFractionalBits(settings["subpixelValue"])
-
-    return stereo
-
-def initialize_pipeline(pipeline, settings):
-    def configure_cam(cam, size_x:int, size_y:int, fps:float):
-        cap = dai.ImgFrameCapability()
-        cap.size.fixed((size_x, size_y))
-
-        cap.fps.fixed(fps)
-        return cam.requestOutput(cap, True)
-    queues = {}
-    input_queues = {}
-    output_settings = settings["output_settings"]
-
-    if output_settings["left"]:
-        monoLeft = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
-        monoLeftOut = configure_cam(monoLeft, settings["stereoResolution"]["x"], settings["stereoResolution"]["y"], settings["FPS"])
-
-    if output_settings["right"]:
-        monoRight = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
-        monoRightOut = configure_cam(monoRight, settings["stereoResolution"]["x"], settings["stereoResolution"]["y"], settings["FPS"])
-
-    if output_settings["rgb"]:
-        color = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_A)
-        colorOut = configure_cam(color, settings["rgbResolution"]["x"], settings["rgbResolution"]["y"], settings["FPS"])
-
-
-
-    if output_settings["left"] or output_settings["left_raw"]: input_queues["left_input_control"] = monoLeft.inputControl.createInputQueue()
-    if output_settings["right"] or output_settings["right_raw"]: input_queues["right_input_control"] = monoRight.inputControl.createInputQueue()
-
-
-
-    if output_settings["depth"] or output_settings["disparity"]:
-        stereo = set_stereo_node(pipeline, settings)
-        monoLeftOut.link(stereo.left)
-        monoRightOut.link(stereo.right)
-
-
-
-    if output_settings["sync"]:
-        sync = pipeline.create(dai.node.Sync)
-        sync.setRunOnHost(settings['sync_on_host'])  # Can also run on device
-
-        if output_settings["depth"]:
-            stereo.syncedLeft.link(sync.inputs["left"])
-            stereo.syncedRight.link(sync.inputs["right"])
-            stereo.disparity.link(sync.inputs["disparity"])
-            stereo.depth.link(sync.inputs["depth"])
-        else:
-            monoLeftOut.link(sync.inputs["left"])
-            monoRightOut.link(sync.inputs["right"])
-
-        if output_settings["rgb"]:
-            colorOut.link(sync.inputs["rgb"])
-
-        if output_settings["left_raw"]: monoLeft.raw.link(sync.inputs["left_raw"])
-        if output_settings["right_raw"]: monoRight.raw.link(sync.inputs["right_raw"])
-
-        if output_settings.get("rgb_raw", False):
-            raise NotImplementedError("RGB raw frames not implemented")
-            # if output_settings["rgb_raw"]: color.raw.link(sync.inputs["rgb_raw"])
-
-        queues["sync"] = sync.out.createOutputQueue()
-
-    elif not output_settings["sync"]:
-        if output_settings["depth"]:
-            syncedLeftQueue = stereo.syncedLeft.createOutputQueue()
-            syncedRightQueue = stereo.syncedRight.createOutputQueue()
-            disparityQueue = stereo.disparity.createOutputQueue()
-            depthQueue = stereo.depth.createOutputQueue()
-
-            queues['left'] = syncedLeftQueue
-            queues['right'] = syncedRightQueue
-            queues['disparity'] = disparityQueue
-            queues['depth'] = depthQueue
-
-        else:
-            queues['left'] = monoLeftOut.createOutputQueue()
-            queues['right'] = monoRightOut.createOutputQueue()
-
-        if output_settings["rgb"]:
-            queues["rgb"] = colorOut.createOutputQueue()
-
-        if output_settings["left_raw"]: queues['left_raw'] = monoLeft.raw.createOutputQueue()
-        if output_settings["right_raw"]: queues['right_raw'] = monoRight.raw.createOutputQueue()
-        if output_settings.get("rgb_raw", False):
-            # queues['rgb_raw'] = color.raw.createOutputQueue()
-            raise NotImplementedError("RGB raw frames not implemented")
-
-
-    return pipeline, queues, input_queues
+from utils.capture_universal import initialize_capture, finalise_capture, count_output_streams
+from pipelines.dai3_stereo_pipeline import initialize_pipeline
 
 def parseArguments():
     # PARSE ARGUMENTS
